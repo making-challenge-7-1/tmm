d6a3b7d341125d9302b48735ee2a5657bb8ec97c
jiwoong Moon <mjw92@kakao.com>
2021-11-29 16:45:50 +0900

    crawling code add

--- static/scripts/main.js
+++ static/scripts/main.js
@@ -1,12 +1,24 @@
 function showList() {
-  const recommend = document.getElementById('movie-recommend');
-  const m_list = document.getElementById('movie-list');
-
-  if(recommend.style.display === 'none')  {
-    recommend.style.display = 'block';
-    m_list.style.display = 'none';
-  }else {
-    recommend.style.display = 'none';
-    m_list.style.display = 'flex';
+  const recommend = document.getElementById("movie-recommend");
+  const m_list = document.getElementById("movie-list");
+  if (recommend.style.display == "none") {
+    recommend.style.display = "block";
+    m_list.style.display = "none";
+  } else {
+    recommend.style.display = "none";
+    m_list.style.display = "flex";
   }
-} 
+}
+
+// function showDetail() {
+//   const m_detail = document.getElementById("movie-detail");
+//   const m_list = document.getElementById("movie-list");
+
+//   if (m_detail.style.display == "none") {
+//     m_detail.style.display = "block";
+//     m_list.style.display = "none";
+//   } else {
+//     m_detail.style.display = "none";
+//     m_list.style.display = "flex";
+//   }
+// }
