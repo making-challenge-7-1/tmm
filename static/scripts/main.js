43fe0be24696b8d8285006e20a91b700a0934bf9
jiwoong Moon <mjw92@kakao.com>
2021-11-30 16:42:24 +0900

    server part init

--- static/scripts/main.js
+++ static/scripts/main.js
@@ -1,3 +1,35 @@
+$(document).ready(() => {
+  getRecommendList();
+});
+
+function getRecommendList() {
+  $.ajax({
+    type: "GET",
+    url: "/recommend/top",
+    data: {},
+    success: function (response) {
+      console.log(response);
+
+      let happy_img = `<img src="" alt="" class="movie-card-poster"
+                onclick="showDetail()">`;
+
+      let angry_img = `<img src="" alt="" class="movie-card-poster"
+                onclick="showDetail()">`;
+
+      let sad_img = `<img src="" alt="" class="movie-card-poster"
+                onclick="showDetail()">`;
+
+      let move_img = `<img src="" alt="" class="movie-card-poster"
+                onclick="showDetail()">`;
+
+      $("#head-happy").append(happy_img);
+      $("#head-angry").append(angry_img);
+      $("#head-sad").append(sad_img);
+      $("#head-move").append(move_img);
+    },
+  });
+}
+
 function showList() {
   let recommend = document.getElementById("movie-recommend");
   let m_list = document.getElementById("movie-list");
@@ -12,15 +44,5 @@ function showList() {
 }
 
 function showDetail() {
-  // const m_detail = document.getElementById("movie-detail");
-  // const m_list = document.getElementById("movie-list");
-
-  // if (m_detail.style.display == "none") {
-  //   m_detail.style.display = "block";
-  //   m_list.style.display = "none";
-  // } else {
-  //   m_detail.style.display = "none";
-  //   m_list.style.display = "flex";
-  // }
   location.href = "/detail";
 }
