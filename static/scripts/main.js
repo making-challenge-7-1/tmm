c98f066c98234acb7e0765aa3e7851bf120dd2ad
jwm92 <mjw92@kakao.com>
2021-12-05 20:37:27 +0900

    ui update

--- static/scripts/main.js
+++ static/scripts/main.js
@@ -81,7 +81,7 @@ function showList() {
   let m_list = document.getElementById("movie-list");
 
   if (recommend.style.display == "none") {
-    recommend.style.display = "flex";
+    recommend.style.display = "block";
     m_list.style.display = "none";
   } else {
     recommend.style.display = "none";
