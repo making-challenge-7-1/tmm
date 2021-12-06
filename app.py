1b29c90c0a0128c7510000dada3d02f780f8482d
dlxorb96 <80591023+dlxorb96@users.noreply.github.com>
2021-12-06 20:44:51 +0900

    register 기능 구현

--- app.py
+++ app.py
@@ -9,9 +9,10 @@ client = MongoClient('mongodb://test:test@52.79.33.194', 27017)
 db = client.dbsparta
 
 # client = MongoClient("localhost", 27017)
-
 # db = client.dbMovie
+
 movieList = db.tp7
+review = db.review
 
 app.secret_key = "ABCDEFG"
 
@@ -20,35 +21,73 @@ app.secret_key = "ABCDEFG"
 def init():
     return render_template("index.html")
 
-
-# happy_list = list(movieList.find({"genre": "신남"}, {"_id": False}))
-
-
-# happy_title = happy_list["title"]
-
-# print(happy_title)
+# register(회원가입)
+@app.route("/register")
+def register_get():
+    return render_template("register.html")
 
 
-# cut score
-# def cutScore(item):
-#     if((item) > 6):
-#         return item
+@app.route("/register", methods=["POST"])
+def register_post():
+    ID_receive = request.form["ID_give"]
+    password_receive = request.form["password_give"]
+
+    doc ={'id' : ID_receive,
+          'password': password_receive}
+    db.register.insert_one(doc)
+
+    return jsonify({'msg':'저장이 완료되었습니다'})
+
+#
+# @app.route("/register", methods=["POST"])
+# def register():
+#     email_receive = request.form["email_give"]
+#     password_receive = request.form["password_give"]
+#
+#     doc = {'email': email_receive,
+#            'password': password_receive}
+#     db.register.insert_one(doc)
+#
+#     return render_template("register.html")
+
+# login(로그인)
+@app.route("/login")
+def login():
+    
+    return render_template("login.html")
+
+
+@app.route("/movies", methods=["GET"])
+def movie_list():
+    return render_template("movies.html")
+
 
 # detail page view
-@app.route("/detail", methods=['GET'])
+@app.route("/detail", methods=["GET"])
 def detail():
     return render_template("detail.html")
 
-tests = list(movieList.find({'genre':'신남'},{'_id':False}))
-# print(tests)
 
-def cutName(n) :
-    score = float(n["score"])
-    if score > 6 :
-        return n
+# # get reviews
+# @app.route("/reviews", methods=["GET"])
+# def get_reviews():
+#     reviews = list(db.reviews.find({}, {"_id": False}))
+#     return jsonify({'target_reviews': reviews})
 
+#update reviews
+    # @app.route("/reviews/update", methods=["POST"])
+    # def update_reviews():
+    #     title_receive = request.form['title_give']
+    #     writer_receive = request.form['writer_give']
+    #     review_receive = request.form['review_give']
 
-
+    # doc = {
+    #     "movie": movie_receive,
+    #     "writer": writer_receive,
+    #     "review": review_receive
+    # }
+    # db.review.insert_one(doc)
+    # return
 
 # top 4 movie get 기분별 하나씩 랜덤하게 가져오기
 @app.route("/recommend/top", methods=["GET"])
@@ -58,33 +97,29 @@ def get_recommend_top():
 
     try:
         happy_list = list(movieList.find({"genre": "신남"}, {"_id": False}))
-        scoreCut_happy = list(map(cutName, happy_list))
-        print(scoreCut_happy)
-        # scoreCut = happy_list(map(cutScore(),))
-        # if scoreCut > 5.00 :
+        today_happy = random.sample(happy_list, 1)[0]
 
-        today_happy = random.sample(scoreCut_happy, 1)[0]
         happy_title = today_happy["title"]
         happy_img = today_happy["img_url"]
         recommend_top.append(happy_title)
         recommend_top.append(happy_img)
 
         angry_list = list(movieList.find({"genre": "화남"}, {"_id": False}))
-        today_angry = random.sample(angry_list, 1)[0]        
+        today_angry = random.sample(angry_list, 1)[0]
         angry_title = today_angry["title"]
         angry_img = today_angry["img_url"]
         recommend_top.append(angry_title)
         recommend_top.append(angry_img)
 
         sad_list = list(movieList.find({"genre": "우울"}, {"_id": False}))
-        today_sad = random.sample(sad_list, 1)[0]        
+        today_sad = random.sample(sad_list, 1)[0]
         sad_title = today_sad["title"]
         sad_img = today_sad["img_url"]
         recommend_top.append(sad_title)
         recommend_top.append(sad_img)
 
         move_list = list(movieList.find({"genre": "떠남"}, {"_id": False}))
-        today_move = random.sample(move_list, 1)[0]        
+        today_move = random.sample(move_list, 1)[0]
         move_title = today_move["title"]
         move_img = today_move["img_url"]
         recommend_top.append(move_title)
@@ -104,10 +139,9 @@ def get_recommend_list():
         genre_receive = request.form["genre_name"]
         # print(genre_receive)
 
-        movie_list = list(movieList.find({"genre": genre_receive}, {"_id": False}))
-        # scoreCut_movie_list = list(map(cutName, movie_list))
-        # print(scoreCut_movie_list)
-
+        movie_list = list(
+            movieList.find({"genre": genre_receive}, {"_id": False}).sort("score", -1)
+        )
         # print(movie_list)
 
     except Exception:
@@ -115,6 +149,20 @@ def get_recommend_list():
         return jsonify({"error"})
 
     return jsonify({"movie_list": movie_list})
+
+
+# get all genre movie list
+@app.route("/find/all", methods=["GET"])
+def find_all_movie():
+    try:
+        movie_list_all = list(movieList.find({}, {"_id": False}).sort("score", -1))
+        # print(movie_list_all)
+
+    except Exception:
+
+        return jsonify({"error to find movies"})
+
+    return jsonify({"movie_list": movie_list_all})
 
 
 # get movie detail
@@ -126,7 +174,6 @@ def find_movie_detail():
 
         target = movieList.find_one({"title": title_receive}, {"_id": False})
         print(target)
-
 
     except Exception as e:
         return {"message": "failed to search"}, 401
