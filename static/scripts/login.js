11ec979f5e9d2143b8cd4b79db6ce7150e1904a1
jiwoong Moon <mjw92@kakao.com>
2021-12-07 15:11:41 +0900

    login update

--- static/scripts/login.js
+++ static/scripts/login.js
@@ -1,36 +1,39 @@
 function sign_in() {
-    let username = $("#input-username").val()
-    let password = $("#input-password").val()
+  let username = $("#input-username").val();
+  let password = $("#input-password").val();
 
-    if (username == "") {
-        $("#help-id-login").text("아이디를 입력해주세요.")
-        $("#input-username").focus()
-        return;
-    } else {
-        $("#help-id-login").text("")
-    }
+  if (username == "") {
+    $("#help-id-login").text("아이디를 입력해주세요.");
+    $("#input-username").focus();
+    return;
+  } else {
+    $("#help-id-login").text("");
+  }
 
-    if (password == "") {
-        $("#help-password-login").text("비밀번호를 입력해주세요.")
-        $("#input-password").focus()
-        return;
-    } else {
-        $("#help-password-login").text("")
-    }
-    $.ajax({
-        type: "POST",
-        url: "/sign_in",
-        data: {
-            username_give: username,
-            password_give: password
-        },
-        success: function (response) {
-            if (response['result'] == 'success') {
-                $.cookie('mytoken', response['token'], {path: '/'});
-                window.location.replace("/")
-            } else {
-                alert(response['msg'])
-            }
-        }
-    });
+  if (password == "") {
+    $("#help-password-login").text("비밀번호를 입력해주세요.");
+    $("#input-password").focus();
+    return;
+  } else {
+    $("#help-password-login").text("");
+  }
+  $.ajax({
+    type: "POST",
+    url: "/sign_in",
+    data: {
+      username_give: username,
+      password_give: password,
+    },
+    success: function (response) {
+      if (response["result"] == "success") {
+        window.location.replace("/");
+      } else {
+        alert(response["msg"]);
+      }
+    },
+  });
+}
+
+function to_sign_up() {
+  window.location.href("/register");
 }
