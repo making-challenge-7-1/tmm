f3336b1e7eb9011256697456aab00ce7588bde7d
jiwoong Moon <mjw92@kakao.com>
2021-12-09 13:31:20 +0900

    movie list modify comment delete add

--- app.py
+++ app.py
@@ -10,13 +10,12 @@ app = Flask(__name__)
 
 client = MongoClient("localhost", 27017)
 db = client.dbMovie
-# db = client.dbsparta
 
 movieList = db.tp7
 comment = db.comment
 
 
-app.secret_key = "ABCDEFG"
+app.secret_key = "WFLIX"
 
 
 # main page view
@@ -40,7 +39,7 @@ def login():
 # all movie list view
 @app.route("/movies", methods=["GET"])
 def movie_list():
-    return render_template("movies.html")
+    return render_template("movielist.html")
 
 
 # detail page view
@@ -83,7 +82,9 @@ def sign_in():
     if result is not None:
         session["username"] = result.get("username")
 
-        return jsonify({"result": "success", "msg": "로그인 성공", "username": username_receive}) #username = sessionstorage에 저장 될 예정
+        return jsonify(
+            {"result": "success", "msg": "로그인 성공", "username": username_receive}
+        )  # username = sessionstorage에 저장 될 예정
     # 찾지 못하면
     else:
         return jsonify({"result": "fail", "msg": "아이디/비밀번호가 일치하지 않습니다."})
@@ -94,38 +95,6 @@ def sign_in():
 def logout():
     session.pop("username", None)
     return redirect(url_for("init"))
-
-#comment 불러오기
-@app.route("/comments", methods=["POST"])
-def get_comments():
-    title_receive = request.form["title_give"]
-    comments = list(
-        db.comment.find({"title": title_receive}, {"_id": False})
-    )  # title로 comment 조회
-    return jsonify({"target_comments": comments})
-
-
-# update comments
-@app.route("/comments/update", methods=["POST"])
-def update_comments():
-
-    title_receive = request.form["title_give"]
-    ID_receive = request.form["ID_give"]
-    comment_receive = request.form["comment_give"]
-
-    if ID_receive == "": # ID를 찾아 올 수 없는 경우
-        session.pop("username", None)
-        return jsonify({"msg": "로그인을 다시 해주세요"})
-    elif comment_receive == "": # comment가 없는 경우
-        return jsonify({"msg": "내용을 작성해주세요"})
-    else:
-        doc = {
-            "title": title_receive,
-            "ID": ID_receive,
-            "comment": comment_receive,
-        }  # title, ID, comment로 저장
-        db.comment.insert_one(doc)
-        return jsonify({"msg": "등록 완료"})
 
 
 # top 4 movie get 기분별 하나씩 랜덤하게 가져오기
@@ -163,8 +132,7 @@ def get_recommend_top():
         recommend_top.append(move_title)
         recommend_top.append(move_img)
 
-    except Exception as e:
-        print(recommend_top)
+    except Exception:
         return jsonify({"ERROR: fail to get top items"})
 
     return jsonify({"recommendTop": recommend_top})
@@ -212,15 +180,32 @@ def find_all_movie_abc():
     return jsonify({"movie_list": movie_list_all})
 
 
+@app.route("/find/movie", methods=["POST"])
+def find_movie_by_title():
+    try:
+        title_receive = request.form["title_give"]
+        movie_list_all = list(
+            movieList.find({"title": {"$regex": title_receive}}, {"_id": False}).sort(
+                "title", 1
+            )
+        )
+
+    except Exception:
+
+        return jsonify({"error to find movies"})
+
+    return jsonify({"movie_list": movie_list_all})
+
+
 # get movie detail
 @app.route("/find", methods=["POST"])
 def find_movie_detail():
     try:
         title_receive = request.form["title_give"]
-        print(title_receive)
+        # print(title_receive)
 
         target = movieList.find_one({"title": title_receive}, {"_id": False})
-        print(target)
+        # print(target)
 
     except Exception as e:
         return {"message": "failed to search"}, 401
@@ -228,24 +213,54 @@ def find_movie_detail():
     return jsonify({"movie_data": target})
 
 
-# #새 코멘트 db에 저장
-# @app.route("/comments/write", methods=["POST"])
-# def write_comment():
-#     try:
-#         username_receive = request.form["username_give"]
-#         comment_receive = request.form["comment_give"]
-#         title_receive = request.form["title_give"]
-#         target = {
-#             "title": title_receive,
-#             "username": username_receive,
-#             "comment": comment_receive,
-#         }
-#         comments.insert_one(target)
-#
-#     except Exception as e:
-#         return {"message": "failed to search"}, 401
-#
-#     return jsonify({"comment_data": target, "msg": "comment delivery"})
+# comment 관련 code
+
+## comment 불러오기
+@app.route("/comments", methods=["POST"])
+def get_comments():
+    title_receive = request.form["title_give"]
+
+    # title로 comment 조회
+    comments = list(db.comment.find({"title": title_receive}, {"_id": False}))
+    return jsonify({"target_comments": comments})
+
+
+## comment 쓰기
+@app.route("/comments/update", methods=["POST"])
+def update_comments():
+
+    title_receive = request.form["title_give"]
+    ID_receive = request.form["ID_give"]
+    comment_receive = request.form["comment_give"]
+
+    if ID_receive == "":  # ID를 찾아 올 수 없는 경우
+        session.pop("username", None)
+        return jsonify({"msg": "로그인을 다시 해주세요"})
+    elif comment_receive == "":  # comment가 없는 경우
+        return jsonify({"msg": "내용을 작성해주세요"})
+    else:
+        doc = {
+            "title": title_receive,
+            "ID": ID_receive,
+            "comment": comment_receive,
+        }  # title, ID, comment로 저장
+        db.comment.insert_one(doc)
+        return jsonify({"msg": "등록 완료"})
+
+
+## comment 지우기
+@app.route("/comments/delete", methods=["POST"])
+def delete_comment():
+
+    title_receive = request.form["title_give"]
+    ID_receive = request.form["ID_give"]
+    doc = {
+        "title": title_receive,
+        "ID": ID_receive,
+    }
+    db.comment.delete_one(doc)
+
+    return jsonify({"msg": "삭제 완료"})
 
 
 if __name__ == "__main__":
--- static/scripts/detail.js
+++ static/scripts/detail.js
@@ -1,23 +1,29 @@
 let title = localStorage.getItem("title");
-let ID = sessionStorage.getItem("username")
-$('#comment-list').empty();
+let ID = sessionStorage.getItem("username");
 
-$.ajax({
-  type: "POST",
-  url: "/find",
-  data: { title_give: title },
-  success: function (response) {
-    let movie_data = response["movie_data"];
-    console.log(movie_data);
+$(document).ready(() => {
+  get_detail();
+  get_comments();
+});
 
-    let poster = movie_data["img_url"];
-    let title = movie_data["title"];
-    let score = movie_data["score"];
-    let desc = movie_data["desc"];
-    let url = movie_data["url"];
-    let genre = movie_data["genre"];
+//영화 정보 가져오기
+function get_detail() {
+  $.ajax({
+    type: "POST",
+    url: "/find",
+    data: { title_give: title },
+    success: function (response) {
+      let movie_data = response["movie_data"];
+      console.log(movie_data);
 
-    let temp_html = `<div class="content detail">
+      let poster = movie_data["img_url"];
+      let title = movie_data["title"];
+      let score = movie_data["score"];
+      let desc = movie_data["desc"];
+      let url = movie_data["url"];
+      let genre = movie_data["genre"];
+
+      let temp_html = `<div class="content detail">
                             <input type="image" src="${poster}" width="202" height="290" style="float: left"/>
                             <div class="boxPadding">
                             <h1>${title}</h1>
@@ -30,25 +36,28 @@ $.ajax({
                           </div>
                           `;
 
-    $("#movie_detail").append(temp_html);
-  },
-});
-
-// function get_comments() {}
+      $("#movie_detail").append(temp_html);
+    },
+  });
+}
 //comment 받아오기
-$.ajax({
-  type: "POST",
-  url: "/comments",
-  data: {title_give: title},
-  success: function (response) {
-    let comment_data = response["target_comments"];
-    console.log(comment_data);
+function get_comments() {
+  $("#comment-list").empty();
 
-    for (let i = 0; i < comment_data.length; i++) {
-      let ID = comment_data[i]["ID"]
-      let comment = comment_data[i]["comment"]
+  $.ajax({
+    type: "POST",
+    url: "/comments",
+    data: { title_give: title },
+    success: function (response) {
+      let comment_data = response["target_comments"];
+      console.log(comment_data);
 
-      let temp_html = `<div id="comment-item" style="margin: 40px 0">
+      for (let i = 0; i < comment_data.length; i++) {
+        let ID = comment_data[i]["ID"];
+        let comment = comment_data[i]["comment"];
+        let temp_html = ``;
+        if (ID == sessionStorage["username"]) {
+          temp_html = `<div id="comment-item" style="margin: 20px 0">
                         <article class="media">
                           <figure class="media-left" style="margin: 1rem 2rem">
                             <p class="image is-64x64">
@@ -59,35 +68,86 @@ $.ajax({
                             <div class="content">
                               <p>
                                 <strong>${ID}</strong>
-                                <br />
-                                ${comment}
+                              </p>
+                              <p id="comment_${i}">
+                              ${comment}
                               </p>
                             </div>
                           </div>
                           <div class="media-right">
-                            <button class="delete"></button>
+                            <button class="delete" onclick="delete_comment(${i})"></button>
                           </div>
+                          
                         </article>
                       </div>`;
+        } else {
+          temp_html = `<div id="comment-item" style="margin: 20px 0">
+                        <article class="media">
+                          <figure class="media-left" style="margin: 1rem 2rem">
+                            <p class="image is-64x64">
+                              <i class="fas fa-user" style="font-size: 48px"></i>
+                            </p>
+                          </figure>
+                          <div class="media-content" id="review">
+                            <div class="content">
+                              <p>
+                                <strong>${ID}</strong>
+                              </p>
+                              <p>
+                                ${comment}
+                              </p>
+                                
+                            </div>
+                          </div>
+                          <div class="media-right">
+                            <button class="delete is-hidden"></button>
+                          </div>
+                          
+                        </article>
+                      </div>`;
+        }
 
-      $("#comment-list").append(temp_html);
-    }
-  },
-});
+        $("#comment-list").append(temp_html);
+      }
+    },
+  });
+}
 
 function add_comment() {
-  let comment = $('#new-post').val()
+  let comment = $("#new-post").val();
   $.ajax({
     type: "POST",
     url: "/comments/update",
-    data: {title_give: title, ID_give: ID, comment_give: comment},
+    data: { title_give: title, ID_give: ID, comment_give: comment },
     success: function (response) {
-      if (response["msg"] == "내용을 작성해주세요"){ //내용이 없을경우
-        alert(response["msg"])
-      }else { //ID가 없을경우 등록 안됨 but 모두 충족시 정상 작동
-      alert(response["msg"]);
+      if (response["msg"] == "내용을 작성해주세요") {
+        //내용이 없을경우
+        alert(response["msg"]);
+      } else {
+        //ID가 없을경우 등록 안됨 but 모두 충족시 정상 작동
+        alert(response["msg"]);
+        window.location.reload();
+      }
+    },
+  });
+}
+
+function cancel_comment() {
+  document.getElementById("new-post").value = "";
+}
+
+function delete_comment(num) {
+  console.log(num);
+  // let del_comment = $("#comment_" + String(num)).text();
+  // console.log(del_comment);
+
+  $.ajax({
+    type: "POST",
+    url: "/comments/delete",
+    data: { title_give: title, ID_give: ID },
+    success: function () {
+      get_comments();
       window.location.reload();
-      };
-    }
-  })
+    },
+  });
 }
